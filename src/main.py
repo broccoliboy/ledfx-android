29cea9234cb91564ae655205ce87f7b9f72c7a29
Sam Martin <broccoliboy@gmail.com>
2025-11-03 21:57:26 -0600

    update permissions to only request RECORD_AUDIO

--- src/main.py
+++ src/main.py
@@ -14,10 +14,7 @@ from leanback import is_leanback, serve as serve_leanback
 logger = logging.getLogger('ledfx-android')
 
 permissions_list = [
-    Permission.RECORD_AUDIO,
-    Permission.MODIFY_AUDIO_SETTINGS,
-    Permission.READ_EXTERNAL_STORAGE,
-    Permission.WRITE_EXTERNAL_STORAGE,
+    Permission.RECORD_AUDIO
 ]
 
 
@@ -47,37 +44,35 @@ def validate_permissions():
     """Ensures all required permissions have been granted. If no, request them. If user denies, show a toast and shut down app.
     """
     
-    event = threading.Event()  # use this to signal from lambda callback
+    event = threading.Event()
+    
+    def permissions_callback(permissions, results):
+        # Ensure we have all required permissions to run LedFx
+        if not all(results):
+            
+            Toast = autoclass('android.widget.Toast')
+            String = autoclass('java.lang.String')
+            mActivity = autoclass('org.kivy.android.PythonActivity').mActivity
+            msg = 'Please enable required permissions in app settings.'
+            
+            mActivity.runOnUiThread(
+                lambda: Toast.makeText(
+                    mActivity,
+                    cast('java.lang.CharSequence', String(msg)),
+                    Toast.LENGTH_LONG
+                ).show()
+            )
+
+        event.set()
 
     # Trigger permission dialogues
     request_permissions(
         permissions_list,
-        lambda permissions, results: event.set()
+        permissions_callback
     )
     
-    # Wait for the lambda callback to be called. This happens after user accepts or rejects permission dialogues.
+    # Wait for user to respond to permission requests
     event.wait()
-    
-    # Ensure we have all required permissions to run LedFx
-    if not all([check_permission(p) for p in permissions_list]):
-        
-        Toast = autoclass('android.widget.Toast')
-        String = autoclass('java.lang.String')
-        mActivity = autoclass('org.kivy.android.PythonActivity').mActivity
-        msg = 'Please enable required permissions in app settings.'
-        
-        mActivity.runOnUiThread(
-            lambda: Toast.makeText(
-                mActivity,
-                cast('java.lang.CharSequence', String(msg)),
-                Toast.LENGTH_LONG
-            ).show()
-        )
-
-        time.sleep(1)  # give the toast time to show
-        
-        # Shut down app
-        # raise RuntimeError('Required permissions not granted')
     
 
 if __name__ == '__main__':
