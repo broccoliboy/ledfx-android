33f8ccfc5f42f7a7b79dd652314b494e795ec3d5
Sam Martin <broccoliboy@gmail.com>
2025-11-03 21:57:59 -0600

    always use app storage for config

--- src/ledfx_bootstrap.py
+++ src/ledfx_bootstrap.py
@@ -6,7 +6,7 @@ import logging
 import os
 import sys
 
-from android.storage import app_storage_path, primary_external_storage_path
+from android.storage import app_storage_path
 
 from leanback import is_leanback
 from ports import LEDFX_PORT_LEANBACK, WEBVIEW_PORT
@@ -18,24 +18,15 @@ def start_ledfx():
     sys.platform = 'linux'  # Force sys.platform to 'linux' for compatibility
     
     from ledfx.__main__ import main as ledfx_main
-    from ledfx.config import CONFIG_DIRECTORY
         
     port = LEDFX_PORT_LEANBACK if is_leanback() else WEBVIEW_PORT
-    
-    try:
-        # try to use external storage first
-        config_dir = os.path.join(primary_external_storage_path(), CONFIG_DIRECTORY)
-        os.makedirs(config_dir, exist_ok=True)
-    except:
-        # fall back to app storage directory if external storage is inaccessible
-        config_dir = app_storage_path()
     
     # Reduce logging of noisy modules
     logging.getLogger('kivy.jnius.reflect').setLevel(logging.INFO)
     logging.getLogger('ledfx.api').setLevel(logging.INFO)
     
     sys.argv += [
-        f'--config={config_dir}',
+        f'--config={app_storage_path()}',
         f'--port={port}',
         '--offline',
     ]
