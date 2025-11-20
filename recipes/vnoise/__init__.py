33c3d598cf315bc74fcc8df9289423093bddc8d9
Sam Martin <broccoliboy@gmail.com>
2025-11-19 21:54:22 -0600

    vnoise recipe formatting

--- recipes/vnoise/__init__.py
+++ recipes/vnoise/__init__.py
@@ -1,12 +1,14 @@
 from pythonforandroid.recipe import PyProjectRecipe
 
+
 class VnoiseRecipe(PyProjectRecipe):
-    '''vnoise is pure python but latest version on pypi uses pkg_resources, which is 
+    """Vnoise is pure python but latest version on pypi uses pkg_resources, which is 
     deprecated and throws an exception in latest python versions. We can use this fork until
     the issue is resolved upstream.
-    '''
-    
+    """
+
     version = 'ff10442'
     url = 'https://github.com/broccoliboy/vnoise/archive/{version}.zip'
+
 
 recipe = VnoiseRecipe()
